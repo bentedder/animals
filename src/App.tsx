2ac089f40ed03e65d0edacb36f19b1135193aa09
bentedder <bentedder@gmail.com>
2023-02-06 14:18:37 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrap>
+      <h1>Guess the animal</h1>
+      <Button variant="contained">Start!</Button>
+    </Wrap>
   );
 }
+
+const Wrap = styled.div`
+  padding: 16px;
+`;
 
 export default App;
